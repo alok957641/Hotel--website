b98cad68f86b82770e2b84952d4bfb1d2fa234ca
alok957641 <rajalok957641@gmail.com>
2025-11-15 09:04:43 +0530

    c

---
+++ src/app.js
@@ -0,0 +1,11 @@
+
+
+let navlnk = document.querySelector('#navlink');
+
+let bars = document.querySelector('#micon');
+
+bars.addEventListener("click", ()=>{
+
+    navlnk.classList.toggle("left-[0px]");
+    
+})
